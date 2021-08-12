553db44c1af581c1c68c8c96967fa298748f4729
Rami Menai <rami.menai@outlook.com>
2021-08-12 13:28:34 +0300

    Fixed DELAY str bug

--- bots/config.py
+++ bots/config.py
@@ -6,7 +6,7 @@ from pathlib import Path
 class Settings:
     id = environ.get("NOMICS_COIN_ID")
 
-    reload_delay = environ.get("DELAY", 5)
+    reload_delay = int(environ.get("DELAY", 5))
     debug = environ.get("DEBUG", "").lower() == "true"
 
 
